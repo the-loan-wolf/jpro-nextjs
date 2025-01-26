b43d7288cf02e441fa8a7f2ae1f05666c7254cb3
rishav <rishavb518@gmail.com>
2025-01-26 23:06:52 +0530

    removed unused code from 'add more skill field' button feature

--- app/ui/resumeEntry/SkillInfo.tsx
+++ app/ui/resumeEntry/SkillInfo.tsx
@@ -1,11 +1,9 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import InputField from "./InputField";
 import { addNewField } from "@/app/utils/Utils";
 
 const SkillInfo = () => {
   const [skillFieldCount, setSkillFieldCount] = useState(1);
-  const skillRef = useRef<HTMLDivElement>(null);
-  const skillBtn = useRef<HTMLButtonElement>(null);
 
   function clickHandler(e: React.MouseEvent<HTMLButtonElement>) {
     addNewField(e, skillFieldCount, setSkillFieldCount);
@@ -13,7 +11,7 @@ const SkillInfo = () => {
 
   return (
     <div id="skillsInfo" className="py-3 border-b-2">
-      <div id="skillInfoChild" ref={skillRef}>
+      <div id="skillInfoChild">
         <InputField id="skillName" labelName="Skill Name" />
         <InputField id="skillSource" labelName="Where you learned it from" />
         <InputField id="skillEx" labelName="Experience Time" />
@@ -23,7 +21,6 @@ const SkillInfo = () => {
         type="button"
         className="p-3 bg-lime-200 m-3 rounded-xl hover:-translate-y-1 hover:scale-110 hover:bg-lime-300 transition ease-in-out"
         onClick={clickHandler}
-        ref={skillBtn}
       >
         Add more Skill
       </button>
