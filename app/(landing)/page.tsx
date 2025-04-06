89c3b91e9e70b839ec02ee7bb1c40b59d48eeb7f
rishav <rishavb518@gmail.com>
2025-04-06 09:09:57 +0530

    centered the button on the landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center h-screen">
       <p>This will be our landing page!</p>
       <Link href="/app">
         <button className="min-w-[130px] h-10 text-white px-2.5 py-1 font-bold cursor-pointer transition-all duration-300 ease-in-out relative inline-block outline-none rounded-md border-none bg-[#3a86ff] shadow-[0_5px_#4433ff] hover:shadow-[0_3px_#4433ff] hover:top-[1px] active:shadow-[0_0_#4433ff] active:top-[5px]">
