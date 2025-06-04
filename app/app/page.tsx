61f016c574b9369ae1ba82e9a6af25cd344a62f1
rishav <rishavb518@gmail.com>
2025-06-04 22:51:33 +0530

    work in progress

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,7 @@
+'use client';
 import UserProfileBox from "../ui/UserProfileBox";
 import { getDocument } from "../utils/firebase-fn";
-import { headers } from 'next/headers'
+// import { headers } from "next/headers";
 import React, { PureComponent } from "react";
 import { FixedSizeList as List } from "react-window";
 import InfiniteLoader from "react-window-infinite-loader";
@@ -19,12 +20,13 @@ const LOADING = 1;
 const LOADED = 2;
 const itemStatusMap = {};
 
-const isItemLoaded = (index) => !!itemStatusMap[index];
-const loadMoreItems = (startIndex, stopIndex) => {
+const isItemLoaded = (index: number) => !!itemStatusMap[index];
+
+const loadMoreItems = (startIndex: number, stopIndex: number) => {
   for (let index = startIndex; index <= stopIndex; index++) {
     itemStatusMap[index] = LOADING;
   }
-  return new Promise((resolve) =>
+  return new Promise<void>((resolve) =>
     setTimeout(() => {
       for (let index = startIndex; index <= stopIndex; index++) {
         itemStatusMap[index] = LOADED;
@@ -51,24 +53,26 @@ class Row extends PureComponent {
   }
 }
 
-export default async function App() {
+export default function App() {
   // we are doing this to opt out of static rendering
-  const headersList = await headers()
-  const userAgent = headersList.get('user-agent')
+  // const headersList = await headers();
+  // const userAgent = headersList.get("user-agent");
 
-  let userProfiles: UserDocument[] = [];
+  // let userProfiles: UserDocument[] = [];
 
-  try {
-    userProfiles = await getDocument() as UserDocument[];
-  } catch (error) {
-    console.error("Failed to fetch user profiles:", error);
-    // return an error component or fallback
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-red-500">Failed to load profiles. Please try again later.</p>
-      </div>
-    );
-  }
+  // try {
+  //   userProfiles = (await getDocument()) as UserDocument[];
+  // } catch (error) {
+  //   console.error("Failed to fetch user profiles:", error);
+  //   // return an error component or fallback
+  //   return (
+  //     <div className="flex justify-center items-center h-screen">
+  //       <p className="text-red-500">
+  //         Failed to load profiles. Please try again later.
+  //       </p>=
+  //     </div>
+  //   );
+  // }
 
   return (
     // <main className="mt-5 flex justify-center min-h-screen">
@@ -79,7 +83,7 @@ export default async function App() {
     //       <UserProfileBox
     //         key={user.id}
     //         userData={{
-    //           uid: user.id,
+    //        =   uid: user.id,
     //           pic: user.profilePicEle,
     //           name: `${user.resumeFName} ${user.resumeLName}`,
     //           occupation: user.compPost,
@@ -90,23 +94,23 @@ export default async function App() {
     //   </div>
     // </main>
     <InfiniteLoader
-          isItemLoaded={isItemLoaded}
+      isItemLoaded={isItemLoaded}
+      itemCount={1000}
+      loadMoreItems={loadMoreItems}
+    >
+      {({ onItemsRendered, ref }) => (
+        <List
+          className="List"
+          height={150}
           itemCount={1000}
-          loadMoreItems={loadMoreItems}
+          itemSize={30}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+          width={300}
         >
-          {({ onItemsRendered, ref }) => (
-            <List
-              className="List"
-              height={150}
-              itemCount={1000}
-              itemSize={30}
-              onItemsRendered={onItemsRendered}
-              ref={ref}
-              width={300}
-            >
-              {Row}
-            </List>
-          )}
-        </InfiniteLoader>
+          {Row}
+        </List>
+      )}
+    </InfiniteLoader>
   );
 }
