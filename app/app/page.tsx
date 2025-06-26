4d13f2e8383dc06b22a53f423ab6b0c46167bb0b
the-loan-wolf <rishavb518@gmail.com>
2025-06-26 15:23:10 +0530

    added more columns in the grid

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -87,7 +87,7 @@ export default function App() {
         {loading ? (
           <Loading />
         ) : listings && listings.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 justify-items-center gap-y-4">
+          <div className="grid grid-cols-2 sm:grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-y-4">
             {listings &&
               listings.map((listing) => (
                 <UserProfileBox
