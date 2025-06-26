059986b4a693358fd432e3cea39900fcbdc0fcbf
the-loan-wolf <rishavb518@gmail.com>
2025-06-26 13:03:20 +0530

    added pagination

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import UserProfileBox from "../ui/UserProfileBox";
-import { getDocument } from "../utils/firebase-fn";
-import { headers } from 'next/headers'
+import { db, getDocument } from "../utils/firebase-fn";
+import { headers } from "next/headers";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  startAfter,
+  where,
+} from "firebase/firestore";
 
 // Define the document type
 interface UserDocument {
@@ -12,43 +27,98 @@ interface UserDocument {
   salary: string;
 }
 
-export default async function App() {
-  // we are doing this to opt out of static rendering
-  const headersList = await headers()
-  const userAgent = headersList.get('user-agent')
+export default function App() {
+  const [listings, setListings] = useState<
+    { id: string; data: DocumentData }[] | null
+  >(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchListing] =
+    useState<QueryDocumentSnapshot<DocumentData, DocumentData> | null>(null);
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        console.log("entered into fetchListings");
+        const listingRef = collection(db, "resumes");
+        const q = query(listingRef, limit(8));
+        const querySnap = await getDocs(q);
+        console.log(querySnap);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchListing(lastVisible);
+        const listings: { id: string; data: DocumentData }[] = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listing");
+      }
+    }
 
-  let userProfiles: UserDocument[] = [];
+    fetchListings();
+  }, []);
 
-  try {
-    userProfiles = await getDocument() as UserDocument[];
-  } catch (error) {
-    console.error("Failed to fetch user profiles:", error);
-    // return an error component or fallback
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-red-500">Failed to load profiles. Please try again later.</p>
-      </div>
-    );
+  async function onFetchMoreListings() {
+    try {
+      const listingRef = collection(db, "resumes");
+      const q = query(listingRef, startAfter(lastFetchedListing), limit(4));
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchListing(lastVisible);
+      const listings: { id: string; data: DocumentData }[] = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings((prevState) => [...(prevState ?? []), ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch listing");
+    }
   }
 
   return (
-    <main className="mt-5 flex justify-center min-h-screen">
-      <div
-        className="flex justify-evenly flex-wrap gap-4 max-w-5xl"
-      >
-        {userProfiles.map((user) => (
-          <UserProfileBox
-            key={user.id}
-            userData={{
-              uid: user.id,
-              pic: user.profilePicEle,
-              name: `${user.resumeFName} ${user.resumeLName}`,
-              occupation: user.compPost,
-              salary: user.salary,
-            }}
-          />
-        ))}
+    <main>
+      <div className="mt-5 flex justify-center">
+        <div className="flex justify-evenly flex-wrap gap-4 max-w-5xl">
+          {loading ? (
+            <p>loading...</p>
+          ) : listings && listings.length > 0 ? (
+            <>
+              {listings &&
+                listings.map((listing) => (
+                  <UserProfileBox
+                    key={listing.id}
+                    userData={{
+                      uid: listing.id,
+                      pic: listing.data.profilePicEle,
+                      name: `${listing.data.resumeFName} ${listing.data.resumeLName}`,
+                      occupation: listing.data.compPost,
+                      salary: listing.data.salary,
+                    }}
+                  />
+                ))}
+            </>
+          ) : (
+            <p>There are no current offers</p>
+          )}
+        </div>
       </div>
+      {lastFetchedListing && (
+        <div className="flex justify-center items-center">
+          <button
+            onClick={onFetchMoreListings}
+            className="bg-white px-3 py-1.5 text-gray-700 border border-gray-300 mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out"
+          >
+            Load more
+          </button>
+        </div>
+      )}
     </main>
   );
 }
