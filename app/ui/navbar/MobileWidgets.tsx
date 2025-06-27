40fe482423195d0859380a36d7218203060c0101
rishav <rishavb518@gmail.com>
2025-06-27 23:29:31 +0530

    feat: added working search in mobile

--- app/ui/navbar/MobileWidgets.tsx
+++ app/ui/navbar/MobileWidgets.tsx
@@ -1,4 +1,9 @@
-import { useState } from "react";
+import { db } from "@/app/utils/firebase-fn";
+import { lastListing, resumeListings } from "@/app/utils/globalStates";
+import { collection, DocumentData, endAt, getDocs, limit, orderBy, query, startAt } from "firebase/firestore";
+import { useSetAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function MobileWidgets({
   searchBoxToggle,
@@ -10,9 +15,60 @@ export default function MobileWidgets({
   menuToggle: () => void;
 }) {
   const [inputValue, setInputValue] = useState<string>("");
+  const setResults = useSetAtom(resumeListings);
+    const setLastFetchListing = useSetAtom(lastListing);
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+
+  async function searchUsersByName(namePrefix: string) {
+      const usersRef = collection(db, "resumes");
+      const q = query(
+        usersRef,
+        orderBy("resumeFName"),
+        startAt(namePrefix),
+        endAt(namePrefix + "\uf8ff")
+      );
+  
+      const querySnapshot = await getDocs(q);
+      return querySnapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }));
+    }
+  
+    useEffect(() => {
+      const delayDebounce = setTimeout(() => {
+        if (inputValue.length >= 2) {
+          searchUsersByName(inputValue).then((users) => setResults(users));
+        } else {
+          async function fetchListings() {
+            try {
+              const listingRef = collection(db, "resumes");
+              const q = query(
+                listingRef,
+                orderBy("timestamp", "desc"),
+                limit(10)
+              );
+              const querySnap = await getDocs(q);
+              const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+              setLastFetchListing(lastVisible);
+              const listings: { id: string; data: DocumentData }[] = [];
+              querySnap.forEach((doc) => {
+                return listings.push({
+                  id: doc.id,
+                  data: doc.data(),
+                });
+              });
+              setResults(listings);
+            } catch (error) {
+              toast.error("Could not fetch listing");
+            }
+          }
+          fetchListings();
+        }
+      }, 300); // debounce
+  
+      return () => clearTimeout(delayDebounce);
+    }, [inputValue]);
+
   return (
     <>
       <div
