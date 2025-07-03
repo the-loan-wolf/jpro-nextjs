5cb9d0785057d348de1face68d72a875553c43d8
rishav <rishavb518@gmail.com>
2025-07-03 12:01:21 +0530

    convert homepage into react server component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
 import UserProfileBox from "../ui/UserProfileBox";
 import { db } from "../utils/firebase-fn";
-import Loading from "./loading";
 import {
   collection,
   DocumentData,
@@ -12,93 +7,21 @@ import {
   limit,
   orderBy,
   query,
-  QueryDocumentSnapshot,
-  startAfter,
-  where,
 } from "firebase/firestore";
-import { useAtom } from "jotai";
-import { lastListing, resumeListings } from "../utils/globalStates";
 import Carousel from "../ui/Carousel";
+import LoadMore from "../ui/LoadMore";
 
-// Define the document type
-interface UserDocument {
-  id: string;
-  profilePicEle: string;
-  resumeFName: string;
-  resumeLName: string;
-  compPost: string;
-  salary: string;
-}
-
-export default function App() {
-  const [listings, setListings] = useAtom(resumeListings);
-  const [loading, setLoading] = useState(true);
-  const [lastFetchedListing, setLastFetchListing] = useAtom(lastListing);
-
-  useEffect(() => {
-    async function fetchListings() {
-      try {
-        const listingRef = collection(db, "resumes");
-        const q = query(listingRef, orderBy("timestamp", "desc"), limit(10));
-        const querySnap = await getDocs(q);
-        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-        setLastFetchListing(lastVisible);
-        const listings: { id: string; data: DocumentData }[] = [];
-        querySnap.forEach((doc) => {
-          return listings.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        setListings(listings);
-        setLoading(false);
-      } catch (error) {
-        toast.error("Could not fetch listing");
-      }
-    }
-    const saved = sessionStorage.getItem("listings");
-    if (listings) {
-      saved && setListings(JSON.parse(saved));
-      setLoading(false);
-    } else {
-      fetchListings();
-    }
-  }, []);
-
-  async function onFetchMoreListings() {
-    try {
-      const listingRef = collection(db, "resumes");
-      const q = query(
-        listingRef,
-        orderBy("timestamp", "desc"),
-        startAfter(lastFetchedListing),
-        limit(10)
-      );
-      const querySnap = await getDocs(q);
-      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-      setLastFetchListing(lastVisible);
-      const listings: { id: string; data: DocumentData }[] = [];
-      querySnap.forEach((doc) => {
-        return listings.push({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-      setListings((prevState) => [...(prevState ?? []), ...listings]);
-      setLoading(false);
-    } catch (error) {
-      toast.error("Could not fetch listing");
-    }
-  }
-
-  // Save listings before unmount
-  useEffect(() => {
-    return () => {
-      if (listings) {
-        sessionStorage.setItem("listings", JSON.stringify(listings));
-      }
-    };
-  }, [listings]);
+export default async function App() {
+  const listingRef = collection(db, "resumes");
+  const q = query(listingRef, orderBy("timestamp", "desc"), limit(10));
+  const querySnap = await getDocs(q);
+  const listings: { id: string; data: DocumentData }[] = [];
+  querySnap.forEach((doc) => {
+    return listings.push({
+      id: doc.id,
+      data: doc.data(),
+    });
+  });
 
   return (
     <main>
@@ -106,38 +29,23 @@ export default function App() {
         <Carousel />
       </div>
       <div className="mt-5 max-w-6xl mx-auto px-3">
-        {loading ? (
-          <Loading />
-        ) : listings && listings.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-y-4">
-            {listings &&
-              listings.map((listing) => (
-                <UserProfileBox
-                  key={listing.id}
-                  userData={{
-                    uid: listing.id,
-                    pic: listing.data.profilePicEle,
-                    name: `${listing.data.resumeFName} ${listing.data.resumeLName}`,
-                    occupation: listing.data.compPost,
-                    salary: listing.data.salary,
-                  }}
-                />
-              ))}
-          </div>
-        ) : (
-          <p>There are no current offers</p>
-        )}
-      </div>
-      {lastFetchedListing && (
-        <div className="flex justify-center items-center">
-          <button
-            onClick={onFetchMoreListings}
-            className="bg-white px-3 py-1.5 text-gray-700 border border-gray-300 mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out"
-          >
-            Load more
-          </button>
+        <div className="grid grid-cols-2 sm:grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center gap-y-4">
+          {listings &&
+            listings.map((listing) => (
+              <UserProfileBox
+                key={listing.id}
+                userData={{
+                  uid: listing.id,
+                  pic: listing.data.profilePicEle,
+                  name: `${listing.data.resumeFName} ${listing.data.resumeLName}`,
+                  occupation: listing.data.compPost,
+                  salary: listing.data.salary,
+                }}
+              />
+            ))}
         </div>
-      )}
+      </div>
+      <LoadMore />
     </main>
   );
 }
--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -3,7 +3,7 @@ import { Instagram, Music, Twitter, Facebook, Youtube } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200 py-12 px-6">
+    <footer className="bg-white border-t border-gray-200 py-12 px-6 mt-3">
       <div className="max-w-6xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Resumes Section */}
